5972d4b249aa2f3f8acc5a7f9804abb960d8fd2d
Jorupbek <a.jorupbek@gmail.com>
2022-04-10 23:43:07 +0300

    add thumbnail

--- plyart_project/settings.py
+++ plyart_project/settings.py
@@ -14,6 +14,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sorl.thumbnail',
 
     # my apps
     'pages.apps.PagesConfig',
